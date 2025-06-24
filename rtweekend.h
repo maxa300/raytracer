5b5c493d0788c6b1b1a8675f56f9d1851a32667d
maxa300 <maxosashi@proton.me>
2025-06-23 21:40:30 -0500

    not sure what I did here

--- rtweekend.h
+++ rtweekend.h
@@ -6,12 +6,13 @@
 #define RTWEEKEND_H
 
 #include <cmath>
+#include <cstdlib>
 #include <iostream>
 #include <limits>
 #include <memory>
 
-// C++ std usings
 
+// C++ std usings
 using std::make_shared;
 using std::shared_ptr;
 
@@ -20,9 +21,16 @@ const double infinity = std::numeric_limits<double>::infinity();
 const double pi = 3.14159265358979323846;
 
 // Utility Functions
+inline double degrees_to_radians(double degrees){
+    return degrees * pi / 180.0;
+}
 
-inline double degrees_to_radians(double degrees) {
-  return degrees * pi / 180;
+inline double random_double(){
+    return std::rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+    return min + (max-min)*random_double();
 }
 
 // Common Headers
--- vec3.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by max on 6/16/25.
-//
-
-#include "vec3.h"
